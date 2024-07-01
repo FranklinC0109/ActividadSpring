2f4d55687aa2917c75351d266aa6f1e86a230cb0
FranklinC0109 <161394292+FranklinC0109@users.noreply.github.com>
2024-07-01 13:26:26 -0500

    Se agrega etiquetas

--- src/main/java/com/unir/actividad/controllers/MatriculaController.java
+++ src/main/java/com/unir/actividad/controllers/MatriculaController.java
@@ -6,6 +6,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import com.unir.actividad.enums.EStatusReponse;
 import com.unir.actividad.services.MatriculaService;
 import com.unir.actividad.services.UsuarioService;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/matricula")
 public class MatriculaController {
--- src/main/java/com/unir/actividad/controllers/PersonaController.java
+++ src/main/java/com/unir/actividad/controllers/PersonaController.java
@@ -8,6 +8,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import com.unir.actividad.entities.StandardResponse;
 import com.unir.actividad.enums.EStatusReponse;
 import com.unir.actividad.services.PersonaService;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/persona")
 public class PersonaController {
